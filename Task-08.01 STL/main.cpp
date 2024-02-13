2543e3d99adced77e4b1f6fb6b0f3544ef1a7f39
Margaret Alade <alade.margaret@yandex.ru>
2024-02-13 06:52:30 +0300

    Initial commit

--- Task-08.01 STL/main.cpp
+++ Task-08.01 STL/main.cpp
@@ -6,9 +6,25 @@
 //
 
 #include <iostream>
+#include <set>
+#include <vector>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    std::vector<int> vec {1, 1, 2, 5, 6, 1, 2, 4};
+    std::set<int> st;
+    std::cout << "[IN]: ";
+    for (const auto& elem : vec) {
+        std::cout << elem << " ";
+    }
+    std::sort(vec.begin(), vec.end());
+    auto it = std::unique(vec.begin(), vec.end());
+    vec.erase(it, vec.end());
+    std::cout << "\n";
+    std::cout << "[OUT]: ";
+    for (const auto& elem : vec) {
+        std::cout << elem << " ";
+    }
+    
+    
     return 0;
 }
